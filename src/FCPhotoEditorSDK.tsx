bc953a9cc7f3277cbbb9c46dd519a26628a49a9d
Dave Laszczak <dlaszczak@gmail.com>
2021-09-29 10:34:42 -0500

    comment out FC component to prevent confusion continued.

--- src/FCPhotoEditorSDK.tsx
+++ src/FCPhotoEditorSDK.tsx
@@ -1,24 +1,23 @@
-// import { UIEvent, PhotoEditorSDKUI } from 'photoeditorsdk'
-// import React from 'react'
+import { UIEvent, PhotoEditorSDKUI } from 'photoeditorsdk'
+import React from 'react'
 
-// export const PhotoEditorSDK = (props: any) => {
-//   React.useEffect(() => {
-//     initEditor()
-//   }, [])
-//   const initEditor = async () => {
-//     const editor = await PhotoEditorSDKUI.init({
-//       container: '#editor',
-//       layout: 'basic',
-//       theme: 'dark',
-//       displayCloseWarning: true,
-//       image: 'http://localhost:3000/assets/example.jpg', // Image url or Image path relative to assets folder
-//       license: ''
-//     })
-//     console.log('PhotoEditorSDK for Web is ready!')
-//     editor.on(UIEvent.EXPORT, imageSrc => {
-//       console.log('Exported ', imageSrc)
-//     })
-//   }
-
-//   return <div id="editor" style={{ width: '100vw', height: '100vh' }} />
-// }
+export const PhotoEditorSDK = (props: any) => {
+  // React.useEffect(() => {
+  //   initEditor()
+  // }, [])
+  // const initEditor = async () => {
+  //   const editor = await PhotoEditorSDKUI.init({
+  //     container: '#editor',
+  //     layout: 'basic',
+  //     theme: 'dark',
+  //     displayCloseWarning: true,
+  //     image: 'http://localhost:3000/assets/example.jpg', // Image url or Image path relative to assets folder
+  //     license: ''
+  //   })
+  //   console.log('PhotoEditorSDK for Web is ready!')
+  //   editor.on(UIEvent.EXPORT, imageSrc => {
+  //     console.log('Exported ', imageSrc)
+  //   })
+  // }
+  // return <div id="editor" style={{ width: '100vw', height: '100vh' }} />
+}
